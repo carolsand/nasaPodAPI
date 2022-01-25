5eee6045abaea5112b7e1c3fcb2f481bc3d436bc
Carol Sanders <carol@carolsanders.org>
2022-01-25 12:07:39 -0800

    Add CRUD for Pod

---
+++ .history/src/controller/pods_20220125112029.js
@@ -0,0 +1,7 @@
+const pod = require("../models/pods");
+
+module.exports = {
+  new: newItem,
+  create,
+  addToRoom,
+};
---
+++ .history/src/controller/pods_20220125112748.js
@@ -0,0 +1,7 @@
+const pod = require("../models/pod");
+
+module.exports = {
+  new: newItem,
+  create,
+  addToRoom,
+};
