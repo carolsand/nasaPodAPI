a9a8026df5ee67f37aa35e61397256c9c3c8bd40
Carol Sanders <carol@carolsanders.org>
2022-01-29 11:59:06 -0800

    Removed unused files. Route updates will delete rating, update rating

--- src/routes.js
+++ src/routes.js
@@ -1,198 +1,225 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 import getPod from "./image_getter";
 import { sequelize } from "./database";
 
 /* --------- CRUD Users ------- */
-// router.post("/users", (req, res) => {
-//   return res.send("POST HTTP method on user resource");
-// });
-
-// router.get("/users", (req, res) => {
-//   return res.send(Object.values(users));
-// });
-
-// router.get("/user/:userId", (req, res) => {
-//   return res.send(users[req.params.userId]);
-// });
-
-// router.put("/user/:userId", (req, res) => {
-//   return res.send(`PUT HTTP method on user/${req.params.userId} resource`);
-// });
-
-// router.delete("/user/:userId", (req, res) => {
-//   return res.send(`DELETE HTTP method on user/${req.params.userId} resource`);
-// });
-
-router.get('/users', (req, res) => {
-  return sequelize.models.user.findAll()
+router.get("/users", (req, res) => {
+  return sequelize.models.user
+    .findAll()
     .then((users) => res.send(users))
     .catch((err) => {
-      console.log('There was an error querying users', JSON.stringify(err))
-      return res.send(err)
+      console.log("There was an error querying users", JSON.stringify(err));
+      return res.send(err);
     });
 });
 
-router.post('/users', (req, res) => {
-  const { firstName, lastName } = req.body
-  return sequelize.models.user.create({ firstName, lastName })
+router.post("/users", (req, res) => {
+  const { firstName, lastName } = req.body;
+  return sequelize.models.user
+    .create({ firstName, lastName })
     .then((user) => res.send(user))
     .catch((err) => {
-      console.log('***There was an error creating a users', JSON.stringify(contact))
-      return res.status(400).send(err)
-    })
+      console.log(
+        "***There was an error creating a users",
+        JSON.stringify(contact)
+      );
+      return res.status(400).send(err);
+    });
 });
 
-router.delete('/user/:id', (req, res) => {
-  const id = parseInt(req.params.id)
-  return sequelize.models.user.findById(id)
+router.delete("/user/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  return sequelize.models.user
+    .findById(id)
     .then((user) => user.destroy({ force: true }))
     .then(() => res.send({ id }))
     .catch((err) => {
-      console.log('***Error deleting users', JSON.stringify(err))
-      res.status(400).send(err)
-    })
+      console.log("***Error deleting users", JSON.stringify(err));
+      res.status(400).send(err);
+    });
 });
 
-router.put('/user/:id', (req, res) => {
-  const id = parseInt(req.params.id)
-  return sequelize.models.user.findById(id)
-  .then((user) => {
-    const { firstName, lastName } = req.body
-    return user.update({ firstName, lastName })
+router.put("/user/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  return sequelize.models.user.findById(id).then((user) => {
+    const { firstName, lastName } = req.body;
+    return user
+      .update({ firstName, lastName })
       .then(() => res.send(user))
       .catch((err) => {
-        console.log('***Error updating user', JSON.stringify(err))
-        res.status(400).send(err)
-      })
-  })
+        console.log("***Error updating user", JSON.stringify(err));
+        res.status(400).send(err);
+      });
+  });
 });
-
 
 /* --------- Read pictures ------- */
 router.get("/pictures", (req, res) => {
   // TODO: Return all pictures, with URL and ID
+  const picture = req.params.id();
 });
 
 router.get("/picture/:pictureId", (req, res) => {
   // TODO: Return all pictures, with URL and ID
-    getPod(req.params.pictureId).then((pod) => {
-        res.send(pod)
-    }).catch(console.log);
+  getPod(req.params.pictureId)
+    .then((pod) => {
+      res.send(pod);
+    })
+    .catch(console.log);
 });
 
-router.get('/pods', (req, res) => {
-  return sequelize.models.pod.findAll()
+router.get("/pods", (req, res) => {
+  return sequelize.models.pod
+    .findAll()
     .then((pods) => res.send(pods))
     .catch((err) => {
-      console.log('There was an error querying pods', JSON.stringify(err))
-      return res.send(err)
+      console.log("There was an error querying pods", JSON.stringify(err));
+      return res.send(err);
     });
 });
 
-router.post('/pods', (req, res) => {
-  const { id, stars } = req.body
-  return sequelize.models.pod.create({ id, stars })
+router.post("/pods", (req, res) => {
+  const { id, stars } = req.body;
+  return sequelize.models.pod
+    .create({ id, stars })
     .then((pod) => res.send(pod))
     .catch((err) => {
-      console.log('***There was an error creating a rating', JSON.stringify(contact))
-      return res.status(400).send(err)
-    })
+      console.log(
+        "***There was an error creating a rating",
+        JSON.stringify(contact)
+      );
+      return res.status(400).send(err);
+    });
 });
 
-router.delete('/pod/:id', (req, res) => {
-  const id = parseInt(req.params.id)
-  return sequelize.models.pod.findById(id)
+router.delete("/pod/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  return sequelize.models.pod
+    .findById(id)
     .then((pod) => pod.destroy({ force: true }))
     .then(() => res.send({ id }))
     .catch((err) => {
-      console.log('***Error deleting pod', JSON.stringify(err))
-      res.status(400).send(err)
-    })
+      console.log("***Error deleting pod", JSON.stringify(err));
+      res.status(400).send(err);
+    });
 });
 
-router.put('/pod/:id', (req, res) => {
-  const id = parseInt(req.params.id)
-  return sequelize.models.pod.findById(id)
-  .then((pod) => {
-    const { id, stars } = req.body
-    return rating.update({ id })
+router.put("/pod/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  return sequelize.models.pod.findById(id).then((pod) => {
+    const { id, stars } = req.body;
+    return rating
+      .update({ id })
       .then(() => res.send(pod))
       .catch((err) => {
-        console.log('***Error updating pod', JSON.stringify(err))
-        res.status(400).send(err)
-      })
-    });
+        console.log("***Error updating pod", JSON.stringify(err));
+        res.status(400).send(err);
+      });
   });
+});
 
 /* --------- CRUD Ratings ------- */
 router.post("/user/:userId/picture/:pictureId/rating", async (req, res) => {
   // TODO: create a rating by the given user for the given picture.
-    const { userId, pictureId } = req.params;
-    const user = await sequelize.models.user.findById(userId);
-    if (!user) {
-        console.log('No user with id of', userId);
-        return res.send({});
-    }
-    const pod = await getPod(pictureId);
-    if (!pod) {
-        console.log('No pod with id of', pictureId);
-        return res.send({});
-    }
+  const { userId, pictureId } = req.params;
+  const user = await sequelize.models.user.findById(userId);
+  if (!user) {
+    console.log("No user with id of", userId);
+    return res.send({});
+  }
+  const pod = await getPod(pictureId);
+  if (!pod) {
+    console.log("No pod with id of", pictureId);
+    return res.send({});
+  }
 
-    const stars = req.body.stars;
-    if (!stars) {
-        console.log('No stars in request body');
-        return res.send({});
-    }
+  const stars = req.body.stars;
+  if (!stars) {
+    console.log("No stars in request body");
+    return res.send({});
+  }
 
-    const [rating, created] = await sequelize.models.rating.findOrCreate({
-        where: {user_id: userId, pod_id: podId},
-        defaults: {
-            user_id: userId,
-            pod_id: podId,
-            stars: stars
-        }
-    });
+  const [rating, created] = await sequelize.models.rating.findOrCreate({
+    where: { user_id: userId, pod_id: podId },
+    defaults: {
+      user_id: userId,
+      pod_id: podId,
+      stars: stars,
+    },
+  });
 
-    // If we found an outdated record, update it
-    if (!created && rating.stars !== stars) {
-        await sequelize.models.rating.update({
-            stars: stars
-        },{
-            where: { id: rating.id }
-        });
-    }
-    return res.send(rating);
+  // If we found an outdated record, update it
+  if (!created && rating.stars !== stars) {
+    await sequelize.models.rating.update(
+      {
+        stars: stars,
+      },
+      {
+        where: { id: rating.id },
+      }
+    );
+  }
+  return res.send(rating);
 });
 
-router.get("/user/:userId/picture/:pictureId/rating", (req, res) => {
+router.get("/user/:userId/picture/:pictureId/rating", async (req, res) => {
   // TODO: read the rating by the given user for the given picture.
+  // const { userId, rating } = req.params;
+  const user = await sequelize.models.user.findById(userId);
+  if (!user) {
+    console.log("No user with id of", userId);
+    return res.send({});
+  }
+  const rating = await getPod(rating);
+  if (!rating) {
+    console.log("No rating with id of", rating);
+    return res.send({});
+  }
 });
 
-router.put("/user/:userId/picture/:pictureId/rating", (req, res) => {
+router.put("/user/:userId/picture/:pictureId/rating", async (req, res) => {
   // TODO: update the rating by the given user for the given picture.
+  const user = await sequelize.models.user.findById(userId);
+  if (!user) {
+    console.log("No user with id of", userId);
+    return res.send({});
+  }
+  rating = await getPod(req.params.ratingId);
+  if (!rating) {
+    console.log("No rating with id of", rating);
+    return res.send({});
+  }
 });
 
-router.delete("/user/:userId/picture/:pictureId/rating", (req, res) => {
+router.delete("/user/:userId/picture/:pictureId/rating", async (req, res) => {
   // TODO: delete the rating by the given user for the given picture.
+  const id = await sequelize.models.user.findById(ratingId);
+  return sequelize.models.rating
+    .findById(id)
+    .then((rating) => rating.destroy({ force: true }))
+    .then(() => res.send({ id }))
+    .catch((err) => {
+      console.log("***Error deleting rating", JSON.stringify(err));
+      res.status(400).send(err);
+    });
 });
 
 router.get("/user/:userId/ratings", async (req, res) => {
   // TODO: Get all the user's ratings.
   // Returns the picture name, id and rating for each rating
-    const { userId } = req.params;
-    let results = await sequelize.models.rating.findAll({
-        where: { user_id: userId }
-    });
-    console.log(results);
+  const { userId } = req.params;
+  let results = await sequelize.models.rating.findAll({
+    where: { user_id: userId },
+  });
+  console.log(results);
 
-    // If the result is empty, return an empty list instead
-    if (results && Object.keys(results).length === 0) {
-        return res.send([]);
-    }
-    return res.send(results);
+  // If the result is empty, return an empty list instead
+  if (results && Object.keys(results).length === 0) {
+    return res.send([]);
+  }
+  return res.send(results);
 });
 
 // router.get('/api/ratings', (req, res) => {
