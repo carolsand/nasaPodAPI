44bca5a27717250b1f2c660dbf59974ce9c2a54d
Carol Sanders <carol@carolsanders.org>
2022-01-28 21:07:43 -0800

    This change removed commented code

--- src/models/pod.js
+++ src/models/pod.js
@@ -18,27 +18,3 @@ Pod.init({
   });
   console.log(pod.toJSON());
 })();
-
-// let users = {
-//   1: {
-//     id: "1",
-//     username: "Robin X",
-//   },
-//   2: {
-//     id: "2",
-//     username: "Carol S",
-//   },
-// };
-
-// let messages = {
-//   1: {
-//     id: "1",
-//     text: "Good Day",
-//     userId: "1",
-//   },
-//   2: {
-//     id: "2",
-//     text: "By World",
-//     userId: "2",
-//   },
-// };
